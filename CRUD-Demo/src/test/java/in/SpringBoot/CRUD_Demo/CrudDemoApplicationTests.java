332351a3fc2b1d1fd780f92f3accb0dfabc55386
chaitanyamore003 <chaitanyamore003@gmail.com>
2026-07-02 09:16:13 +0530

    first CRUD Project

---
+++ CRUD-Demo/src/test/java/in/SpringBoot/CRUD_Demo/CrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package in.SpringBoot.CRUD_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
